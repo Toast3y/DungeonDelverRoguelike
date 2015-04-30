4a3990573e7715a6c591812a0b1c3c47c7ed3706
Toast3y <toast3y@gmail.com>
2015-04-30 17:12:11 +0100

    Added Menu Bar
    Will separate this project into another repository.

--- rng.py
+++ rng.py
@@ -3,8 +3,26 @@
 from Tkinter import *
 
 
-##Create the window
+def hello():
+	print "Hello World!"
+
+
+##Create the window and drop down menu
 root = Tk()
+menubar = Menu(root)
+
+##List drop down menus to create cascade menus
+file_menu = Menu(menubar)
+edit_menu = Menu(menubar)
+##Add the cascades to the menubar
+menubar.add_cascade(label="File", menu = file_menu)
+menubar.add_cascade(label="Edit", menu = edit_menu)
+
+##Add commands to each cascade
+#file_menu
+file_menu.add_command(label="Hello", command=hello)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
 
 
 ##Modify root window
@@ -12,5 +30,6 @@ root.title("Roguelike 'Next-Level' Generator")
 root.geometry("640x480")
 
 
-##Start event loop
+## Display menubar and Start event loop
+root.config(menu=menubar)
 root.mainloop()
